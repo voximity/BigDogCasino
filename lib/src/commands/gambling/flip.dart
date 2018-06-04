58d8c615d484938e866c738172c0ad353b04be76
menofcrest <30881172+menofcrest@users.noreply.github.com>
2018-06-03 21:04:28 -0400

    Fixed issues with Flip

--- lib/src/commands/gambling/flip.dart
+++ lib/src/commands/gambling/flip.dart
@@ -14,13 +14,23 @@ class FlipCommand extends Command {
   FlipCommand(this.userService);
 
   Future<void> execute(context, args) async {
-    final bet = await args['bet'].resolve(Resolver.integer());
+    final bet = await args['bet'].resolve(Resolver.integer(min : 1));
     final account = await userService.getAccount(context.message.author.id);
-    final reward = random.nextBool() ? (bet * 2) : (bet * -1);
-    await account.update({'balance': account.balance + reward});
+    final reward = random.nextBool() ? (bet) : (bet * -1);
+    
+    if (bet > account.balance) {
+      await context
+      .respond((message) => message
+        ..content = 'You don\'t have enough 🐶!')
+      .send();
+      return;
+      }
+    else {
     await context
         .respond((message) => message
           ..content = 'you ${reward >= 0 ? 'Won' : 'lost'} ${(reward.abs())}')
         .send();
+     await account.update({'balance': account.balance + reward});   
   }
+}
 }
